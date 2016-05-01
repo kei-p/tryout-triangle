15f7013c574ae50fb984ad64163e0f50d975c51e
Keisuke Ishizawa <muddyapesjm66@gmail.com>
2016-05-01 13:58:43 +0900

    Refactor specs

--- spec/triangle_spec.rb
+++ spec/triangle_spec.rb
@@ -4,62 +4,54 @@ describe Triangle do
   describe '.triangle_type' do
     subject { Triangle.triangle_type(lengths)  }
 
-    describe 'scalene_triangle' do
-      context 'length [2, 3, 4]' do
-        let(:lengths) { [2, 3, 4] }
-        it { expect(subject).to eq('scalene_triangle') }
-      end
-
-      context 'length [4, 6, 8]' do
-        let(:lengths) { [4, 6, 8] }
-        it { expect(subject).to eq('scalene_triangle') }
-      end
+    context 'when length is [2, 3, 4]' do
+      let(:lengths) { [2, 3, 4] }
+      it { is_expected.to eq('scalene_triangle') }
     end
 
-    context 'isosceles_triangle' do
-      context 'length [2, 2, 1]' do
-        let(:lengths) { [2, 2, 1] }
-        it { expect(subject).to eq('isosceles_triangle') }
-      end
-
-      context 'length [1, 2, 2]' do
-        let(:lengths) { [1, 2, 2] }
-        it { expect(subject).to eq('isosceles_triangle') }
-      end
-
-      context 'length [2, 2, 1]' do
-        let(:lengths) { [2, 2, 1] }
-        it { expect(subject).to eq('isosceles_triangle') }
-      end
-
-      context 'length [4, 4, 2]' do
-        let(:lengths) { [4, 4, 2] }
-        it { expect(subject).to eq('isosceles_triangle') }
-      end
+    context 'when length is [4, 6, 8]' do
+      let(:lengths) { [4, 6, 8] }
+      it { is_expected.to eq('scalene_triangle') }
     end
 
-    context 'equilateral_triangle' do
-      context 'length [1, 1, 1]' do
-        let(:lengths) { [1, 1, 1] }
-        it { expect(subject).to eq('equilateral_triangle') }
-      end
+    context 'length [2, 2, 1]' do
+      let(:lengths) { [2, 2, 1] }
+      it { is_expected.to eq('isosceles_triangle') }
     end
 
-    context 'not_triangle' do
-      context 'length [1, 2, 3]' do
-        let(:lengths) { [1, 2, 3] }
-        it { expect(subject).to eq('not_triangle') }
-      end
+    context 'length [1, 2, 2]' do
+      let(:lengths) { [1, 2, 2] }
+      it { is_expected.to eq('isosceles_triangle') }
+    end
 
-      context 'length [2, 4, 6]' do
-        let(:lengths) { [2, 4, 6] }
-        it { expect(subject).to eq('not_triangle') }
-      end
+    context 'length [2, 2, 1]' do
+      let(:lengths) { [2, 2, 1] }
+      it { is_expected.to eq('isosceles_triangle') }
+    end
 
-      context 'length [1, 1, 2]' do
-        let(:lengths) { [1, 1, 2] }
-        it { expect(subject).to eq('not_triangle') }
-      end
+    context 'length [4, 4, 2]' do
+      let(:lengths) { [4, 4, 2] }
+      it { is_expected.to eq('isosceles_triangle') }
+    end
+
+    context 'length [1, 1, 1]' do
+      let(:lengths) { [1, 1, 1] }
+      it { is_expected.to eq('equilateral_triangle') }
+    end
+
+    context 'length [1, 2, 3]' do
+      let(:lengths) { [1, 2, 3] }
+      it { is_expected.to eq('not_triangle') }
+    end
+
+    context 'length [2, 4, 6]' do
+      let(:lengths) { [2, 4, 6] }
+      it { is_expected.to eq('not_triangle') }
+    end
+
+    context 'length [1, 1, 2]' do
+      let(:lengths) { [1, 1, 2] }
+      it { is_expected.to eq('not_triangle') }
     end
   end
 end
